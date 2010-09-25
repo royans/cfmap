8251233bb34ac2e7ec85cc1fa04bf4159ca4b89d
royans <royans@gmail.com>
2010-09-25 06:56:55 +0000

    c

--- bin/startup.sh
+++ bin/startup.sh
@@ -4,21 +4,23 @@
 cd `dirname $0`
 BASE=`pwd`/..
 CONF=$BASE/conf/cfmap.properties
+LOG=$BASE/logs
 source $CONF
+
+mkdir -p $LOG
 
 cp $BASE/conf/cassandra/* $BASE/cassandra/conf
 perl -p -i -e "s#/usr/local/ingenuity/cfmap/cassandra#$BASE/cassandra_data#g" $BASE/cassandra/conf/*
 perl -p -i -e "s#CFMAP_INSERT_SEED_HERE#<Seed>$cassandra_servers</Seed>#g" $BASE/cassandra/conf/*
 
 cd $BASE/cassandra/bin
-perl -p -i -e "s#com.sun.management.jmxremote.port=$cassandra_http_port#com.sun.management.jmxremote.port=$cassandra_jmx_port#g" $BASE/cassandra/bin/*
+perl -p -i -e "s#com.sun.management.jmxremote.port=$cassandra_http_port#com.sun.management.jmxremote.port=$cassandra_jmx_port#g" $BASE/cassandra/bin/*.sh
 mkdir -p $BASE/cassandra_data
 
 if [ -d $BASE/cassandra_data ]
 then
-    $BASE/cassandra/bin/cassandra start 2> /dev/null 
-    java -jar $BASE/lib/jetty-runner.jar --path /cfmap $BASE/lib/cfmap.war 2> /dev/null
+    cd $LOG
+    nohup $BASE/cassandra/bin/cassandra start & > /dev/null 2> /dev/null 
+    nohup java -jar $BASE/lib/jetty-runner.jar --port $cfmap_port --path /cfmap $BASE/lib/cfmap.war & > /dev/null 2> /dev/null
 fi
-
-
 
