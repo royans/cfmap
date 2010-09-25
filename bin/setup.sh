2be0ef687c278bb385205ed999bc6f1fb5176013
royans <royans@gmail.com>
2010-09-25 06:21:17 +0000

    c

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd `dirname $0`
-BASE=`pwd`
+BASE=`pwd`/..
 
 ########################################
 # check for tools
@@ -44,7 +44,7 @@ cd $BASE/tmp
 svn co https://svn.codehaus.org/jetty-contrib/jetty/tags/$JETTYC/
 cd $JETTYC/jetty-runner
 mvn clean install
-cp $BASE/tmp/$JETTYC/jetty-runner/target/jetty-runner*.jar $BASE/output/jetty-runner.jar
+cp $BASE/tmp/$JETTYC/jetty-runner/target/jetty-runner*.jar $BASE/output/lib/jetty-runner.jar
 cd $BASE
 
 ########################################
@@ -53,37 +53,4 @@ cd $BASE
 
 tar -xvzf contrib/apache-cassandra-0.6.4-bin.tar.gz
 rm -rf cassandra-live
-mv apache-cassandra-0.6.4 cassandra-live
-cp contrib/cassandra/* cassandra-live/conf/
-
-# update config to point to local cassandra_data dir
-cd cassandra-live/conf
-perl -p -i -e "s#/usr/local/ingenuity/cfmap/cassandra#$BASE/cassandra_data#g" *
-perl -p -i -e "s#CFMAP_INSERT_SEED_HERE#<Seed>127.0.0.1</Seed>#g" *
-
-cd $BASE/cassandra-live/bin
-perl -p -i -e "s#com.sun.management.jmxremote.port=8080#com.sun.management.jmxremote.port=8081#g" *
-cd $BASE
-mkdir -p $BASE/cassandra_data
-
-
-
-exit
-
-######################################################################################################################
-######################################################################################################################
-######################################################################################################################
-######################################################################################################################
-######################################################################################################################
-
-
-cp $BASE/output/cfmap.war tomcat-live/webapps/
-mv $BASE/output $BASE/build
-
-pid8005=`lsof -ai | grep ":8005" | awk '{print $2}'`; kill -9 $pid8005 2> /dev/null
-pid8080=`lsof -ai | grep ":8080" | awk '{print $2}'`; kill -9 $pid8080 2> /dev/null
-pid8081=`lsof -ai | grep ":8081" | awk '{print $2}'`; kill -9 $pid8081 2> /dev/null
-user=`whoami`
-pgrep -u $user -f cassandra | xargs kill -9 2> /dev/null
-pgrep -u $user -f java | xargs kill -9 2> /dev/null
-
+mv apache-cassandra-0.6.4 $BASE/output/cassandra-live
--- bin/startup.sh
+++ bin/startup.sh
@@ -2,11 +2,22 @@
 
 
 cd `dirname $0`
-BASE=`pwd`
+BASE=`pwd`/..
+CONF=$BASE/conf/cfmap.properties
+source $CONF
+
+exit
+cp $BASE/conf/cassandra/* $BASE/cassandra-live/conf
+perl -p -i -e "s#/usr/local/ingenuity/cfmap/cassandra#$BASE/cassandra_data#g" $BASE/cassandra-live/conf/*
+perl -p -i -e "s#CFMAP_INSERT_SEED_HERE#<Seed>$cassandra_servers</Seed>#g" $BASE/cassandra-live/conf/*
+
+cd $BASE/cassandra-live/bin
+perl -p -i -e "s#com.sun.management.jmxremote.port=$cassandra_http_port#com.sun.management.jmxremote.port=$cassandra_jmx_port#g" $BASE/cassandra-live/bin/*
+mkdir -p $BASE/cassandra_data
+
 
 if [ -d $BASE/cassandra_data ]
 then
-    $BASE/tomcat-live/bin/startup.sh  2> /dev/null
     $BASE/cassandra-live/bin/cassandra start 2> /dev/null 
 fi
 
