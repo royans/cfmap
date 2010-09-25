b6a374c994f3ed970d1c267af6599e343b8e2f12
royans <royans@gmail.com>
2010-09-25 06:34:22 +0000

    c

--- bin/shutdown.sh
+++ bin/shutdown.sh
@@ -6,7 +6,6 @@ BASE=`pwd`
 
 if [ -d $BASE/cassandra_data ]
 then
-    $BASE/tomcat-live/bin/shutdown.sh > /dev/null 2> /dev/null
     user=`whoami`
     pgrep -u $user -f cassandra | xargs kill -9 2> /dev/null > /dev/null
 fi
