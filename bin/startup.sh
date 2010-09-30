4368aa5195c6f528ce15997c6de6e42615e98743
royans <royans@gmail.com>
2010-09-30 05:35:53 +0000

    minor changes

--- bin/startup.sh
+++ bin/startup.sh
@@ -9,6 +9,10 @@ LOG=$BASE/logs
 
 cfmap_port=`cat $CONF | grep ^cfmap_port= | cut -d'=' -f2 | sed -e's/\r//g' | sed -e's/\n//g'`
 cassandra_servers=`cat $CONF | grep ^cassandra_servers= | cut -d'=' -f2  | sed -e's/\r//g' | sed -e's/\n//g'`
+if [ "$cassandra_servers" == "localhost" ]
+then
+    cassandra_servers=`hostname`
+fi
 cassandra_http_port=`cat $CONF | grep ^cassandra_http_port= | cut -d'=' -f2  | sed -e's/\r//g' | sed -e's/\n//g'`
 cassandra_jmx_port=`cat $CONF | grep ^cassandra_jmx_port= | cut -d'=' -f2  | sed -e's/\r//g' | sed -e's/\n//g'`
 
@@ -26,9 +30,10 @@ if [ -d $BASE/cassandra_data ]
 then
     cd $LOG
     chmod +x $BASE/cassandra/bin/cassandra
-    nohup $BASE/cassandra/bin/cassandra start & > /dev/null 2> /dev/null 
+   # nohup $BASE/cassandra/bin/cassandra start & > /dev/null 2> /dev/null 
     _PID1=$!
-    nohup java -jar $BASE/lib/jetty-runner.jar --port $cfmap_port --out $BASE/logs/jetty.out --log $BASE/logs/jetty.log --path /cfmap $BASE/lib/cfmap.war  > /dev/null 2> /dev/null
+    nohup java -jar $BASE/lib/jetty-runner.jar --port $cfmap_port --out $BASE/logs/jetty.out --log $BASE/logs/jetty.log --path /cfmap $BASE/lib/cfmap.war & > /dev/null 2> /dev/null
+    echo "java -jar $BASE/lib/jetty-runner.jar --port $cfmap_port --out $BASE/logs/jetty.out --log $BASE/logs/jetty.log --path /cfmap $BASE/lib/cfmap.war "
     _PID2=$!
 
     echo "$_PID1 $_PID2" > $LOG/running.pid
