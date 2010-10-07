017868bfda5e43fc04ddc19f35c2e4fc843e944a
royans <royans@gmail.com>
2010-10-07 15:27:23 +0000

    minor changes

--- build/upload.sh
+++ build/upload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /home/rkt/cc/cfmap/build
 FILE=`ls -tra ../*tar.gz | grep gz | tail -1`
-./upload.py -u royans -w $1 -p cfmap -s 'nightly build' ../$FILE
-rm ../*.tar.gz
+./upload.py -u royans -w $1 -p cfmap -s 'nightly build' $FILE
+#rm ../*.tar.gz
 
 
 
