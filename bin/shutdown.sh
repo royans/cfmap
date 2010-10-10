9b5e62e6cad22b8a891e62b76fffafacf016d6bc
Gary Franczyk <gkf42@yahoo.com>
2010-10-10 12:29:03 +0800

    Startup and Shutdown can be run from any directory.  Shutdown also shuts down cassandra via pidfile instead of using ps. 	modified:   bin/shutdown.sh 	modified:   bin/startup.sh

--- bin/shutdown.sh
+++ bin/shutdown.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 
-cd `dirname $0`
-BASE=`pwd`/..
+BASE0=`dirname $0`
+BASE=${BASE0}/..
 LOG=$BASE/logs
 
 if [ -d $BASE/cassandra_data ]
 then
     user=`whoami`
-
-    PIDS=`cat $LOG/running.pid`
-    for pid in `echo $PIDS`
-    do
-	kill -9 $pid > /dev/null 2> /dev/null
-    done
-    pgrep -u $user -f cassandra | xargs kill -9 2> /dev/null > /dev/null
+    CFMAP_PID=`cat $LOG/cfmap.pid`
+	kill -9 $CFMAP_PID > /dev/null 2> /dev/null
+	CASSANDRA_PID=`cat $LOG/cassandra.pid`
+	kill -9 $CASSANDRA_PID > /dev/null 2> /dev/null
+	
+	# need to start cassandra in the foreground
+    #pgrep -u $user -f cassandra | xargs kill -9 2> /dev/null > /dev/null
 fi
 
