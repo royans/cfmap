d11666f7ffb95bf7b4b70e5fe08b025fc0a70aa7
Gary Franczyk <gkf42@yahoo.com>
2010-10-09 23:55:42 -0400

    Got rid of "cd" commands in startup, and moved jmx config into command line options instead of modifying cassandra startup scripts.  Moved cassandra chmod into build script instead of startup script. 	modified:   bin/startup.sh 	modified:   build/build.xml

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-cd `dirname $0`
 BASE0=`dirname $0`
 BASE=${BASE0}/..
 CONF=$BASE/conf/cfmap.properties
@@ -28,20 +26,24 @@ then
 	mkdir -p $LOG
 fi
 
+if [ ! -d $BASE/cassandra_data ]
+then
+	mkdir $BASE/cassandra_data
+fi
+
 cp $BASE/conf/cassandra/* $BASE/cassandra/conf
 perl -p -i -e "s#/usr/local/ingenuity/cfmap/cassandra#$BASE/cassandra_data#g" $BASE/cassandra/conf/*
 perl -p -i -e "s#CFMAP_INSERT_SEED_HERE#<Seed>$cassandra_servers</Seed>#g" $BASE/cassandra/conf/*
 
 if [ -d $BASE/cassandra_data ]
 then
-    cd $LOG
-    chmod +x $BASE/cassandra/bin/cassandra
-    nohup $BASE/cassandra/bin/cassandra -Dcom.sun.management.jmxremote.port=$cassandra_jmx_port start & > /dev/null 2> /dev/null 
+	echo "Starting cassandra."
+    $BASE/cassandra/bin/cassandra -Dcom.sun.management.jmxremote.port=$cassandra_jmx_port start > $LOG/cassandra.log 2>&1  &
     _PID1=$!
-    nohup java -jar $BASE/lib/jetty-runner.jar --port $cfmap_port --out $BASE/logs/jetty.out --log $BASE/logs/jetty.log --path /cfmap $BASE/lib/cfmap.war & > /dev/null 2> /dev/null
+	echo "Starting cfmap server."
+    java -jar $BASE/lib/jetty-runner.jar --port $cfmap_port --out $BASE/logs/jetty.out --log $BASE/logs/jetty.log --path /cfmap $BASE/lib/cfmap.war >> $LOG/cfmap.log 2>&1 & 
     _PID2=$!
 
-	# Like to keep the pid files separate so that I can control them individually.
     echo "$_PID1 $_PID2" > $LOG/running.pid  
 	echo "$_PID1" > $LOG/cassandra.pid
 	echo "$_PID2" > $LOG/cfmap.pid
