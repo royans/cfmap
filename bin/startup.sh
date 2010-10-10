6bcdcb5babd27fb258ac58ed1bb8dab98c91eef1
Gary Franczyk <gkf42@yahoo.com>
2010-10-09 23:11:18 -0400

    Cleanup of startup script, moved JMX port setting into command line 	modified:   startup.sh

--- bin/startup.sh
+++ bin/startup.sh
@@ -2,45 +2,48 @@
 
 
 cd `dirname $0`
-BASE=`pwd`/..
+BASE0=`dirname $0`
+BASE=${BASE0}/..
 CONF=$BASE/conf/cfmap.properties
 LOG=$BASE/logs
-#source $CONF > /dev/null 2> /dev/null
 
-cfmap_port=`cat $CONF | grep ^cfmap_port= | cut -d'=' -f2 | sed -e's/\r//g' | sed -e's/\n//g'`
-cassandra_servers=`cat $CONF | grep ^cassandra_servers= | cut -d'=' -f2  | sed -e's/\r//g' | sed -e's/\n//g'`
+cfmap_port=`grep ^cfmap_port= $CONF | cut -d'=' -f2 `
+cassandra_servers=`grep ^cassandra_servers= $CONF | cut -d'=' -f2 `
+
 if [ "$cassandra_servers" == "localhost" ]
 then
     cassandra_servers=`hostname`
 fi
+
 if [ "$cassandra_servers" == "" ]
 then
     cassandra_servers=`hostname`
 fi
 
-cassandra_http_port=`cat $CONF | grep ^cassandra_http_port= | cut -d'=' -f2  | sed -e's/\r//g' | sed -e's/\n//g'`
-cassandra_jmx_port=`cat $CONF | grep ^cassandra_jmx_port= | cut -d'=' -f2  | sed -e's/\r//g' | sed -e's/\n//g'`
+cassandra_http_port=`grep ^cassandra_http_port= $CONF| cut -d'=' -f2 `
+cassandra_jmx_port=`grep ^cassandra_jmx_port= $CONF| cut -d'=' -f2 `
 
-mkdir -p $LOG
+if [ ! -d $LOG ]
+then
+	mkdir -p $LOG
+fi
 
 cp $BASE/conf/cassandra/* $BASE/cassandra/conf
 perl -p -i -e "s#/usr/local/ingenuity/cfmap/cassandra#$BASE/cassandra_data#g" $BASE/cassandra/conf/*
 perl -p -i -e "s#CFMAP_INSERT_SEED_HERE#<Seed>$cassandra_servers</Seed>#g" $BASE/cassandra/conf/*
 
-cd $BASE/cassandra/bin
-perl -p -i -e "s#com.sun.management.jmxremote.port=$cassandra_http_port#com.sun.management.jmxremote.port=$cassandra_jmx_port#g" $BASE/cassandra/bin/*.sh
-mkdir -p $BASE/cassandra_data
-
 if [ -d $BASE/cassandra_data ]
 then
     cd $LOG
     chmod +x $BASE/cassandra/bin/cassandra
-    nohup $BASE/cassandra/bin/cassandra start & > /dev/null 2> /dev/null 
+    nohup $BASE/cassandra/bin/cassandra -Dcom.sun.management.jmxremote.port=$cassandra_jmx_port start & > /dev/null 2> /dev/null 
     _PID1=$!
     nohup java -jar $BASE/lib/jetty-runner.jar --port $cfmap_port --out $BASE/logs/jetty.out --log $BASE/logs/jetty.log --path /cfmap $BASE/lib/cfmap.war & > /dev/null 2> /dev/null
-    echo "java -jar $BASE/lib/jetty-runner.jar --port $cfmap_port --out $BASE/logs/jetty.out --log $BASE/logs/jetty.log --path /cfmap $BASE/lib/cfmap.war "
     _PID2=$!
 
-    echo "$_PID1 $_PID2" > $LOG/running.pid
+	# Like to keep the pid files separate so that I can control them individually.
+    echo "$_PID1 $_PID2" > $LOG/running.pid  
+	echo "$_PID1" > $LOG/cassandra.pid
+	echo "$_PID2" > $LOG/cfmap.pid
 fi
 
